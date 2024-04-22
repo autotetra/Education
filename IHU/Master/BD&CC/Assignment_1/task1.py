760e3e788c2c9366a42126228d53f682a24c137a
cstilidisdev <cstilidisdev@gmail.com>
2024-04-22 10:59:51 +0300

    complete ex_1 BD&CC

--- IHU/Master/BD&CC/Assignment_1/task1.py
+++ IHU/Master/BD&CC/Assignment_1/task1.py
@@ -2,6 +2,7 @@ from mrjob.job import MRJob
 import csv
 from datetime import datetime
 
+
 class PostAuthorComments(MRJob):
     def mapper(self, _, line):
         # Parse the line using csv
@@ -12,8 +13,10 @@ class PostAuthorComments(MRJob):
 
     def reducer(self, key, values):
         comments = list(values)
-        comments.sort(key=lambda x: datetime.strptime(x[1], "%Y-%m-%d"), reverse=True)
+        comments.sort(key=lambda x: datetime.strptime(
+            x[1], "%Y-%m-%d"), reverse=True)
         yield key, (len(comments), comments)
+
 
 if __name__ == '__main__':
     PostAuthorComments.run()
