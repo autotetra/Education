0d0e8bfd7c1970ee9548f216d389d99d3e917dcf
Christos Stylidis <christosstylidis@Christoss-MBP.home>
2024-08-13 09:44:16 +0300

    updates

--- IHU/Master/Dissertation/clipboard/promises.js
+++ IHU/Master/Dissertation/clipboard/promises.js
@@ -1,25 +1,16 @@
-function fetchData(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          reject(new Error(`HTTP error! status: ${response.status}`));
-        }
-        return response.json();
-      })
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
-  });
+import axios from "https://cdn.skypack.dev/axios";
+
+async function getTitle() {
+  try {
+    let response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    console.log(`You could ${response.data.title}`);
+  } catch (error) {
+    console.log(`Error! : ${error}`);
+  }
 }
 
-const url = "https://jsonplaceholder.typicode.com/todos/1";
-fetchData(url)
-  .then((data) => {
-    console.log("Success:", data); // This runs if resolve() was called
-  })
-  .catch((error) => {
-    console.error("Error:", error); // This runs if reject() was called
-  })
-  .finally(() => {
-    console.log("Operation complete."); // This runs regardless of the outcome
-  });
+getTitle();
+
+console.log("Why am I here?");
---
+++ IHU/Master/Dissertation/clipboard/random.js
@@ -0,0 +1,11 @@
+import { add, subtract } from "./modules.js";
+
+let sum = add(2, 3);
+let diff = subtract(5, 4);
+console.log(sum, diff);
+
+const multiline = `christos
+test
+another line`;
+
+console.log(multiline);
