e6cdb653755a034b03ba92e131b52f6754c1990d
Christos Stylidis <christosstylidis@Christoss-MBP.home>
2024-08-18 14:52:21 +0300

    update

--- IHU/Master/Dissertation/CustomerApp/server.js
+++ IHU/Master/Dissertation/CustomerApp/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import posts from "./routes/posts.js";
 
+const port = process.env.PORT || 8000;
 const app = express();
+
+//Body parses middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //setup static folder
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+app.use("/api/posts", posts);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
