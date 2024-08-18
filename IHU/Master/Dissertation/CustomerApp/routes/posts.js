aa0b945238eaa6f1e9e3e2cd7c84b575e273005c
Christos Stylidis <christosstylidis@Christoss-MacBook-Pro.local>
2024-08-18 22:53:16 +0300

    updates

--- IHU/Master/Dissertation/CustomerApp/routes/posts.js
+++ IHU/Master/Dissertation/CustomerApp/routes/posts.js
@@ -10,64 +10,35 @@ let posts = [
 ];
 
 //Get all post
-router.get("/", (req, res) => {
-  const limit = parseInt(req.query.limit);
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-  res.status(200).json(posts);
-});
+router.get("/");
 
 //Get single post
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `A post with the id of ${id} was not found` });
-  }
-
-  res.status(200).json(post);
-});
+router.get("/:id");
 
 //Create new post
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const newPost = {
-    id: posts.length + 1,
-    title: req.body.title,
-  };
-  if (!newPost.title) {
-    return res.status(400).json({
-      msg: "please include a title",
-    });
-  }
-  posts.push(newPost);
-  res.status(201).json(posts);
-});
+router.post("/");
 
 //Update post
-router.put("/:id", (req, res) => {
+router.put("/:id", (req, res, next) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id == id);
   if (!post) {
-    return res.status(400).json({
-      msg: `A post with id:${id} was not found`,
-    });
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
   post.title = req.body.title;
   res.status(200).json(posts);
 });
 
 //Delete post
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res, next) => {
   const id = parseInt(req.params.id);
-  const post = posts.find((post) => (post.id = id));
+  const post = posts.find((post) => post.id == id);
   if (!post) {
-    return res.status(400).json({
-      msg: `A post with id:${id} was not found`,
-    });
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
   posts = posts.filter((post) => post.id !== id);
   res.status(200).json(posts);
--- IHU/Master/Dissertation/CustomerApp/server.js
+++ IHU/Master/Dissertation/CustomerApp/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
 
 const port = process.env.PORT || 8000;
 const app = express();
@@ -10,12 +13,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//Logger middleware
+app.use(logger);
 
 //setup static folder
 //app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/posts", posts);
+app.use(notFound);
+
+//Error handler
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
