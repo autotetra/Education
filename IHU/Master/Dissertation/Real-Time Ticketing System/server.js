b2fa779d837d74179460962761d739a95589bf5e
Christos Stylidis <christosstylidis@Christoss-MacBook-Pro.local>
2024-09-02 11:51:52 +0300

    update dissertation

---
+++ IHU/Master/Dissertation/Real-Time Ticketing System/server.js
@@ -0,0 +1,12 @@
+// server.js
+
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// Middleware to parse incoming requests with JSON payloads
