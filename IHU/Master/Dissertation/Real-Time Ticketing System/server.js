82b507562624146703b583fb7a534c5ce0990276
Christos Stylidis <christosstylidis@Christoss-MBP.home>
2024-09-05 02:06:40 +0300

    update

--- IHU/Master/Dissertation/Real-Time Ticketing System/server.js
+++ IHU/Master/Dissertation/Real-Time Ticketing System/server.js
@@ -17,3 +17,9 @@ app.use(express.json());
 app.get("/posts", (req, res) => {
   res.send("Server is running");
 });
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Error connecting to MongoDB", error));
