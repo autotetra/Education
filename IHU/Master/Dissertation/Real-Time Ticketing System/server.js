63805b0bd77967e4c1044dfe138a71e85b8d24d5
Christos Stylidis <cstilidisdev@gmail.com>
2024-09-15 23:22:34 +0300

    updates

--- IHU/Master/Dissertation/Real-Time Ticketing System/server.js
+++ IHU/Master/Dissertation/Real-Time Ticketing System/server.js
@@ -5,7 +5,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { Server } from "socket.io";
-import http from "http";a
+import http from "http";
 // Serve files from the public directory
 import path from "path";
 import { fileURLToPath } from "url";
